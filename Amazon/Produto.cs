283d06c1ccabab7b4eec875d870c1164799ed9f0
bmlfa <bmlfa@DESKTOP-D5VVI9I>
2023-06-26 16:22:53 -0300

    Adicionar arquivos de projeto.

---
+++ Amazon/Produto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Amazon
+{
+    public class Produto
+    {
+        public string ID { get; set; } 
+        public string Nome { get; set; }    
+        public decimal Preco { get; set; }  
+        public int Estoque { get; set; }
+    }
+}
